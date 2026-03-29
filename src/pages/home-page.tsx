6356350c0e18b732599ed87c9746c1e66e38de9e
martsforever <martsforever@aliyun.com>
2026-03-29 15:24:26 +0800

    feat: 测试代码，验证刷新token排队的行为

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,3 +1,40 @@
+import {useCallback, useState} from "react";
+import Axios from "axios";
+import env from "../AppService/env";
+import {createTokenSaver} from "../AppService/createTokenSaver";
+import {createTokenService} from "../AppService/createTokenService";
+import {Button} from "antd";
+import {login} from "../AppService/login";
+
+const http = Axios.create({ baseURL: env.baseURL });
+
 export default () => {
-  return <>首页</>;
+
+  const [tokenSaver] = useState(() => createTokenSaver("pages"));
+  const [tokenService] = useState(() => createTokenService(tokenSaver));
+
+  const getUser = useCallback(async () => {
+    const resp = await http.get('/users/me', {
+      headers: {
+        Authorization: `Bearer ${await tokenService.getToken()}`
+      }
+    });
+    console.log("用户信息：", resp.data);
+  }, [tokenService]);
+
+  const testRequest = useCallback(async () => {
+    getUser();
+    getUser();
+    getUser();
+  }, [getUser]);
+
+  return <>
+    <Button onClick={testRequest}>
+      请求用户信息
+    </Button>
+    <Button onClick={() => {
+      tokenSaver.clear();
+      login();
+    }}>登出</Button>
+  </>;
 };

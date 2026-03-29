20e4f1e9cd73c1e9222ae92b75af5d2846b758bb
martsforever <martsforever@aliyun.com>
2026-03-29 15:24:12 +0800

    feat: 去掉mock

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import env from './AppService/env';
 import {IndexApp, router} from './layouts/routes';
 import {ConfigProvider} from "antd";
-import {enableMockJs} from "./mock/mock";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<>
@@ -14,4 +13,4 @@ root.render(<>
 
 console.log(env, router);
 
-env.enableMockApi && enableMockJs();
+

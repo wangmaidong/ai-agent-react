152950d6c0a294a9885bccea7056d30e260ca4a4
martsforever <martsforever@aliyun.com>
2026-03-29 16:05:10 +0800

    feat: DynamicPage组件需要监听路由变化，重新加载页面组件

--- src/components/DynamicPage/index.tsx
+++ src/components/DynamicPage/index.tsx
@@ -1,8 +1,7 @@
 import {useParams} from "react-router";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Alert, notification} from "antd";
 import PageSpin from "../PageSpin";
-import {useMounted} from "../../uses/useMounted";
 
 export const DynamicPage = (props: { dirname: string }) => {
 
@@ -15,23 +14,26 @@ export const DynamicPage = (props: { dirname: string }) => {
 
   const [Component, setComponent] = useState(null as null | ((props: any) => any));
 
-  useMounted(async () => {
+  useEffect(() => {
     if (!!routeName) {
-      try {
-        const PageComponent = await import('../../' + props.dirname + '/' + routeName + '-page').then(val => val.default);
-        if (!PageComponent) {
-          notification.error({ message: 'Missing export default in page.' });
+      // console.log(`dynamic page: ${routeName}`);
+      (async () => {
+        try {
+          const PageComponent = await import('../../' + props.dirname + '/' + routeName + '-page').then(val => val.default);
+          if (!PageComponent) {
+            notification.error({ message: 'Missing export default in page.' });
+          }
+          setComponent(() => PageComponent);
+        } catch (e) {
+          setComponent(() => {
+            return () => (
+              <div style={{ padding: '1em' }}>
+                <Alert type="error" message={`页面"${routeName}"不存在`}/>
+              </div>
+            );
+          });
         }
-        setComponent(() => PageComponent);
-      } catch (e) {
-        setComponent(() => {
-          return () => (
-            <div style={{ padding: '1em' }}>
-              <Alert type="error" message={`页面"${routeName}"不存在`}/>
-            </div>
-          );
-        });
-      }
+      })();
     } else {
       setComponent(() => {
         return () => (
@@ -41,7 +43,7 @@ export const DynamicPage = (props: { dirname: string }) => {
         );
       });
     }
-  });
+  }, [routeName, props.dirname]);
 
   return !Component ? <PageSpin/> : <Component {...props as any}/>;
 };

eb09461f4f256f0699266bb100a0126df5e78098
martsforever <martsforever@aliyun.com>
2026-03-29 15:58:16 +0800

    feat: 首页改成使用appService的http发请求

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,26 +1,15 @@
-import {useCallback, useState} from "react";
-import Axios from "axios";
-import env from "../AppService/env";
-import {createTokenSaver} from "../AppService/createTokenSaver";
-import {createTokenService} from "../AppService/createTokenService";
+import {useCallback} from "react";
 import {Button} from "antd";
-import {login} from "../AppService/login";
-
-const http = Axios.create({ baseURL: env.baseURL });
+import {useInjectApp} from "../AppService/useAppService";
 
 export default () => {
 
-  const [tokenSaver] = useState(() => createTokenSaver("pages"));
-  const [tokenService] = useState(() => createTokenService(tokenSaver));
+  const { http, logout } = useInjectApp();
 
   const getUser = useCallback(async () => {
-    const resp = await http.get('/users/me', {
-      headers: {
-        Authorization: `Bearer ${await tokenService.getToken()}`
-      }
-    });
+    const resp = await http.get('/users/me', {});
     console.log("用户信息：", resp.data);
-  }, [tokenService]);
+  }, [http]);
 
   const testRequest = useCallback(async () => {
     getUser();
@@ -32,9 +21,6 @@ export default () => {
     <Button onClick={testRequest}>
       请求用户信息
     </Button>
-    <Button onClick={() => {
-      tokenSaver.clear();
-      login();
-    }}>登出</Button>
+    <Button onClick={logout}>登出</Button>
   </>;
 };

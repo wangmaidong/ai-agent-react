1740a2e13dd317d4ad6e35cd43d7469613be66b7
martsforever <martsforever@aliyun.com>
2026-04-11 14:53:23 +0800

    feat: 恢复mock接口

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,23 @@
 import ReactDOM from 'react-dom/client';
-import {IndexApp} from "./layouts/routes";
-import {ConfigProvider} from "antd";
+import { IndexApp } from './layouts/routes';
+import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
 import './styles/index.scss';
+import { enableMockJs } from './mock/mock';
+import env from './AppService/env';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <ConfigProvider locale={zhCN} theme={{
-    token: {
-      fontFamily: 'PingFang SC'
-    }
-  }}>
+  <ConfigProvider
+    locale={zhCN}
+    theme={{
+      token: {
+        fontFamily: 'PingFang SC',
+      },
+    }}
+  >
     {IndexApp}
   </ConfigProvider>
 );
+
+env.enableMockApi && enableMockJs();

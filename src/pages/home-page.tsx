902935b104ad5ab9acb847eda0e2dc38494f24e3
martsforever <martsforever@aliyun.com>
2026-04-12 16:56:39 +0800

    feat: 依赖加载器以及打包时的依赖分析

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,14 +1,15 @@
-import {useCallback} from "react";
-import {Button} from "antd";
-import {useInjectApp} from "../AppService/useAppService";
+import { useCallback } from 'react';
+import { Button } from 'antd';
+import { useInjectApp } from '../AppService/useAppService';
+import { useMounted } from '../uses/useMounted';
+import { LibraryLoader } from '../utils/LibraryLoader';
 
 export default () => {
-
   const { http, logout } = useInjectApp();
 
   const getUser = useCallback(async () => {
     const resp = await http.get('/users/me', {});
-    console.log("用户信息：", resp.data);
+    console.log('用户信息：', resp.data);
   }, [http]);
 
   const testRequest = useCallback(async () => {
@@ -16,6 +17,11 @@ export default () => {
     getUser();
     getUser();
   }, [getUser]);
+
+  useMounted(async () => {
+    const Echarts = await LibraryLoader.load('echarts');
+    console.log('Echarts', Echarts);
+  });
 
   return (
     <>

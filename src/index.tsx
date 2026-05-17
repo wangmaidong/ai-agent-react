50a5eea2213a008e34cc02eddc96080caa23c00d
martsforever <martsforever@aliyun.com>
2026-05-17 16:18:53 +0800

    feat: 自定义的图片列扩展

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,9 @@ import zhCN from "antd/locale/zh_CN";
 import "./styles/index.scss";
 import { enableMockJs } from "./mock/mock";
 import env from "./AppService/env";
+import { installColumnImage } from "./packages/image/col.image.tsx";
+
+installColumnImage();
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -5,15 +5,10 @@ import { useAutoTable } from "../components/AutoTable/useAutoTable.tsx";
 export const HomePage = () => {
 
   const autoTable = useAutoTable(() => ({
-    module: "user",
+    module: "product",
     columns: [
-      { type: "input", title: "用户名", dataIndex: "username", key: "username", required: true },
-      { type: "input", title: "用户名", dataIndex: "username", key: "username" },
-      {
-        type: "input", title: "用户昵称", dataIndex: "fullName", key: "fullName",
-        width: 200,
-        rules: [{ pattern: /^hello/, message: "必须以hello开头" }],
-      },
+      { type: "image", title: "预览图", dataIndex: "pictureUrl", imgHeight: 50 },
+      { type: "input", title: "商品名称", dataIndex: "name", width: 500 },
     ],
   }));
 

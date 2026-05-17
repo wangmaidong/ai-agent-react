34ea13468b2b7eb86a3665d7529b824bc421fc38
martsforever <martsforever@aliyun.com>
2026-05-17 16:44:02 +0800

    feat: 列扩展，以及AutoTable模块扩展的非js说明

--- src/components/AutoTable/梳理模块的扩展方式.js
+++ src/components/AutoTable/梳理模块的扩展方式.js
@@ -11,9 +11,14 @@ export const globalAutoModuleRegistration = [
   { seq: 1, key: "config", use: useAutoTableConfig },
   { seq: 2, key: "state", use: useAutoTableState },
   { seq: 3, key: "handler", use: useAutoTableHandler },
+  { seq: 4, key: "viteSearch", use: useAutoTableHandler },
 ];
 
 import {globalAutoModuleRegistration} from 'pack'
+
+const index = globalAutoModuleRegistration.findIndex(item => item.key === "viteSearch")
+globalAutoModuleRegistration.splice(index, 1);
+
 globalAutoModuleRegistration.push({
   seq: 99,
   key: "batch_delete",

3974225847f58e6d166c1db47b3e00f91c5b88b1
martsforever <martsforever@aliyun.com>
2026-05-23 14:09:40 +0800

    feat: 备份首页测试页面

--- src/layouts/menus.tsx
+++ src/layouts/menus.tsx
@@ -9,6 +9,7 @@ export const menus: MenuItem[] = [
   { key: "/pages/home", icon: <HomeOutlined />, label: "系统首页" },
   { key: "/pages/resume/resume-template-list", icon: <HomeOutlined />, label: "简历模板" },
   { key: "/pages/resume/resume-user-list", icon: <HomeOutlined />, label: "用户简历" },
+  { key: "/pages/demo/demo-backup", icon: <HomeOutlined />, label: "通用表格" },
 
   // { key: '/pages/book/hotel-list', icon: <CoffeeOutlined/>, label: '酒店预定', },
   //

fe5337dfd7c7197d7794b7013276207a0028e767
martsforever <martsforever@aliyun.com>
2026-05-24 16:24:56 +0800

    feat: 一个按钮演示如何通过tempColumns 修改表格配置

--- src/pages/demo/demo-auto-table-col-types-page.tsx
+++ src/pages/demo/demo-auto-table-col-types-page.tsx
@@ -1,6 +1,7 @@
 import { Card } from "antd";
 import { useAutoTable } from "../../components/AutoTable/useAutoTable.tsx";
 import { PageContainer } from "../../components/PageContainer/PageContainer.tsx";
+import { useMemo } from "react";
 
 export default function Page() {
 
@@ -17,6 +18,26 @@ export default function Page() {
       { type: "datetime", title: "日期时间", dataIndex: "createdAt" },
     ],
   }));
+
+  autoTable.hooks.buttonConfigs.push(useMemo(() => {
+    return {
+      key: "custom",
+      seq: -1,
+      label: "覆盖字段",
+      onClick: () => {
+        autoTable.state.setTempColumns(
+          [
+            { "title": "选项值", "field": "selectVal", "type": "select", "options": [{ "label": "潜在客户", "value": "potential" }, { "label": "门店", "value": "store" }, { "label": "消费者", "value": "consumer" }], "width": "120px", "seq": 0, "fixed": "left" },
+            { "title": "日期", "field": "dateVal", "type": "datetime", "width": "180px", "seq": 1, "fixed": "left" },
+            { "title": "名称", "field": "normalText", "type": "input", "width": "120px", "seq": 2, "fixed": "center" },
+            { "title": "数字", "field": "numberVal", "type": "number", "width": "120px", "seq": 3, "fixed": "center" },
+            { "title": "整数", "field": "count", "type": "number", "width": "120px", "seq": 4, "fixed": "center" },
+            { "title": "日期时间", "field": "createdAt", "type": "datetime", "width": "220px", "seq": 5, "fixed": "center" },
+          ] as any,
+        );
+      },
+    };
+  }, [autoTable.state.setTempColumns]));
 
   return (
     <PageContainer>

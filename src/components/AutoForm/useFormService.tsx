24b93f865271ac1154714d45df30256fd5765254
martsforever <martsforever@aliyun.com>
2026-05-24 10:22:58 +0800

    feat: 实现Vibe填写表单

--- src/components/AutoForm/useFormService.tsx
+++ src/components/AutoForm/useFormService.tsx
@@ -6,6 +6,8 @@ import { useStableCallback } from "../../uses/useStableCallback.tsx";
 import { AutoForm, type iAutoFormInstance, type iAutoFormProps } from "./AutoForm.tsx";
 import type { iAutoColumn } from "../AutoColumn/AutoColumn.utils.tsx";
 import type { PlainObject } from "@peryl/utils/event";
+import { useCopilotService } from "../ChatCopilot/useCopilotService.tsx";
+import { getFormSystemPrompt } from "./getFormSystemPrompt.tsx";
 
 export interface iFormServiceConfig extends Omit<iDrawerServiceConfig, "handleConfirm" | "handleCancel" | "content"> {
   columns: iAutoColumn[],
@@ -19,14 +21,54 @@ export function useFormService() {
 
   const { openDrawer } = useDrawerService();
 
+  const { openCopilotService } = useCopilotService();
+
   const openFormDrawer = useStableCallback((config: iFormServiceConfig) => {
 
     const { columns, record, autoFormProps, ...drawerServiceConfig } = config;
 
     const autoFormRef = { current: null as null | iAutoFormInstance };
 
+    const fillWithCopilot = () => {
+      const { closeModal } = openCopilotService({
+        systemPrompt: getFormSystemPrompt(autoFormRef.current!.availableColumns),
+        handleAiMessage: (message) => {
+          const startTag = "/*---DataStart---*/";
+          const endTag = "/*---DataEnd---*/";
+          if (message.indexOf(endTag) > -1) {
+            const startIndex = message.indexOf(startTag);
+            const endIndex = message.indexOf(endTag);
+            const jsonString = message.slice(startIndex + startTag.length, endIndex);
+            const resultData = JSON.parse(jsonString);
+            console.log(`大模型提取结果：`, resultData);
+            Object.entries(record).forEach(([key, val]) => {
+              if (resultData[key] == null) {resultData[key] = val;}
+            });
+            autoFormRef.current!.form.setFieldsValue(resultData);
+            closeModal();
+          }
+        },
+      });
+    };
+
     return openDrawer({
       ...drawerServiceConfig,
+      drawerProps: {
+        ...drawerServiceConfig.drawerProps,
+        title: "编辑表单",
+      },
+      footerLeft: (
+        <div onClick={fillWithCopilot} className="gradient-text" style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          whiteSpace: "nowrap",
+          cursor: "pointer",
+        }}>
+          <i className="bi bi-soundwave"></i>
+          <span style={{ marginLeft: "6px" }}>Vibe填写表单</span>
+        </div>
+      ),
       content: (
         <AutoForm
           onRef={autoFormRef}
